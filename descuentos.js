fca89f4f642e3a8f8fa27ace4e7810c69dbb24af
ENMANUEL <enmanueljosue2612@hotmail.com>
2021-08-05 11:51:37 -0400

    Hice el codigo para calcular con cupon de descuentos, pero estos se seleccionan

--- descuentos.js
+++ descuentos.js
@@ -1,20 +1,25 @@
 // const precioOriginal= 120;
 // const descuento = 18;
 
-function calcularPrecioConDescuentro(precio, descuento){
-    const porcentajePrecioConDescuento = 100 - descuento;
+function calcularPrecioConDescuentro(precio, descuento, cupon){
+    const descuentototal = descuento + cupon;
+    const porcentajePrecioConDescuento = 100 - descuentototal;
     const precioConDescuento = (precio * porcentajePrecioConDescuento)/100; 
     return precioConDescuento;
 }
 
 function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
-    const priceValue = inputPrice.value;
+    const priceValue = parseInt(inputPrice.value);
     
     const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    const discountValue = parseInt(inputDiscount.value);
 
-    const precioConDescuento = calcularPrecioConDescuentro(priceValue,discountValue);
+    const InputCupon = document.getElementById("InputCupon");
+    const cuponValue = parseInt(InputCupon.value);
+    console.log(typeof cuponValue);
+
+    const precioConDescuento = calcularPrecioConDescuentro(priceValue, discountValue, cuponValue);
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
