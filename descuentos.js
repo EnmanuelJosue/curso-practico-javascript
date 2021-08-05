e9a3f7ec1fef6622b1b7e9c474711cd671b0ce02
ENMANUEL <enmanueljosue2612@hotmail.com>
2021-08-05 14:52:44 -0400

    Por medio de ifs validamos las 2 opciones, ingresando cualquier descuento del producto, y ingresando el descuento con un codigo de descuento

--- descuentos.js
+++ descuentos.js
@@ -29,19 +29,34 @@ function onClickButtonPriceDiscount(){
     
     const inputDiscount = document.getElementById("InputDiscount");
     const discountValue = parseInt(inputDiscount.value);
+    if(Number.isNaN(discountValue)){
+        console.log("Soy Nan");
+    }else{
+        console.log("No soy nan");
+    }
 
     const InputCupon = document.getElementById("InputCupon");
     const cuponValue = InputCupon.value;
     const value = coupons.find((element) => element.name === cuponValue); // funcion find para buscar en array
     if(!value){
-        alert("El cupon de descuento no es valido" + cuponValue);
+        alert("El cupon de descuento no es valido: " + cuponValue);
+        const cuponDescuento = 0;
+        const precioConDescuento = calcularPrecioConDescuentro(priceValue, discountValue, cuponDescuento);
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
     }
-    else{
+    else if(Number.isNaN(discountValue)){ // Number.isNaN(discountValue) para saber cuando es Not a Number (NaN)
         console.log(typeof cuponValue);
+        const discountValue = 0;
         const cuponDescuento = parseInt(value.discount);
         const precioConDescuento = calcularPrecioConDescuentro(priceValue, discountValue, cuponDescuento);
         const resultP = document.getElementById("ResultP");
         resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+    }else{
+        const cuponDescuento = parseInt(value.discount);
+        const precioConDescuento = calcularPrecioConDescuentro(priceValue, discountValue, cuponDescuento);
+        const resultP = document.getElementById("ResultP");
+        resultP.innerText = "El precio con descuento son: $" + precioConDescuento;   
     }
 }
 
