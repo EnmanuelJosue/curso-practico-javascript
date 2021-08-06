c9f0b1dae8c7c769bc3059f17cdd3d77a09c0c9e
ENMANUEL <enmanueljosue2612@hotmail.com>
2021-08-06 15:42:07 -0400

    Creamos el proyecto salarios para calcular la mediana, usamos el arrar.push y tambien usamos array.splice

---
+++ salarios.js
@@ -0,0 +1,84 @@
+const colombia = [
+
+];
+
+colombia.push({
+    name: "Camila",
+    salary: 500,
+});
+colombia.push({
+    name: "Julia",
+    salary: 250,
+});
+colombia.push({
+    name: "Carmela",
+    salary: 550,
+});
+colombia.push({
+    name: "Mike",
+    salary: 1000,
+});
+colombia.push({
+    name: "Luna",
+    salary: 2200,
+});
+colombia.push({
+    name: "Asa",
+    salary: 240,
+});
+colombia.push({
+    name: "Kendra",
+    salary: 1500,
+});
+colombia.push({
+    name: "Lisa",
+    salary: 1750,
+});
+colombia.push({
+    name: "Baily",
+    salary: 800,
+});
+colombia.push({
+    name: "Emma",
+    salary: 650,
+});
+colombia.push({
+    name: "Violet",
+    salary: 1350,
+});
+colombia.push({
+    name: "Francheska",
+    salary: 2400,
+});
+colombia.push({
+    name: "Gina",
+    salary: 1660,
+});
+colombia.push({
+    name: "Angela",
+    salary: 2500,
+});
+colombia.push({
+    name: "Lana",
+    salary: 3100,
+});
+colombia.push({
+    name: "Romi",
+    salary: 360,
+});
+colombia.push({
+    name: "Kenzie",
+    salary: 650,
+});
+colombia.push({
+    name: "Mia",
+    salary: 1400,
+});
+colombia.push({
+    name: "Lela",
+    salary: 950,
+});
+colombia.push({
+    name: "Riley",
+    salary: 690,
+});
