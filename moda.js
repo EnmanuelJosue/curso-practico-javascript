5e7ee66416c17cca5540dbaebd07adeaac1fe821
ENMANUEL <enmanueljosue2612@hotmail.com>
2021-08-06 00:21:45 -0400

    hicimos el codigo para calcular la moda en javascript, con funciones, objetos, arrays, metodo map, object.entries y tambien el .sort

---
+++ moda.js
@@ -0,0 +1,43 @@
+// const lista1 = [
+//     1,
+//     2,
+//     2,
+//     1,
+//     1,
+//     2,
+//     4,
+//     2,
+//     2,
+//     1,
+//     1,
+// ];
+
+// const lista1Count = {};
+
+
+// const lista1Array = Object.entries(lista1Count).sort(
+//     function(elementoA, elementoB){
+//         return elementoA[1] - elementoB[1];
+//     }
+// );
+//const moda = lista1Array[lista1Array.length -1];
+
+function calcularModa(lista1){
+    const lista1Count = {}; // El objeto agrupara el array de menor a mayor los elementos. 
+    lista1.map( // El método map() crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos
+        function(elemento){
+            if(lista1Count[elemento]){
+                lista1Count[elemento] += + 1;
+            }else {
+                lista1Count[elemento] = 1;
+            }            
+        }
+    );
+    const lista1Array = Object.entries(lista1Count).sort(
+        function(elementoA, elementoB){
+            return elementoA[1] - elementoB[1];
+        }
+    );
+    const moda = lista1Array[lista1Array.length -1];
+    return moda;
+}
